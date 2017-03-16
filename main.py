59be3d6067f6ba6adf445139862e31ecd2491dff
archer <1246506786@qq.com>
2017-03-17 00:16:25 +0800

    dontneedpassnexttime

--- main.py
+++ main.py
@@ -30,5 +30,17 @@ repo.index.add(PushFiles)
 # git commit -m 'msg'
 repo.index.commit(sys.argv[1])
 
+# git config credential.helper store
+cr = repo.config_reader()
+# cw = repo.config_writer()
+if cr.has_section('credential') and cr.get_value('credential', 'helper') == u'store':
+    pass
+else:
+    # cw.add_section('credential')
+    try:
+        cr.set_value('credential', 'helper', 'store')
+    except:
+        pass
+
 # git push
 repo.remotes.origin.push(refspec='master:master')
