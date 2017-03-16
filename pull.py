4b6dd55d1a61fe9374ce5603da090347ca9c7dfb
archer <1246506786@qq.com>
2017-03-16 23:41:54 +0800

    pull

--- pull.py
+++ pull.py
@@ -2,8 +2,9 @@
 
 import git
 repo = git.Repo('/tmp/GitNote')
-REMOTE_URL = "https://github.com/csrgxtu/GitNote.git"
-origin = repo.create_remote('origin', REMOTE_URL)
-origin.fetch()
-origin.pull(origin.refs[0].remote_head)
+repo.remotes.origin.pull(refspec='master:master')
+# REMOTE_URL = "https://github.com/csrgxtu/GitNote.git"
+# origin = repo.create_remote('origin', REMOTE_URL)
+# origin.fetch()
+# origin.pull(origin.refs[0].remote_head)
 print "---- DONE ----"
